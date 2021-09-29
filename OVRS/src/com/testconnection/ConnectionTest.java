7d6ab201debaf1f926f606ac9c0eacdde08dc769
Chamath <chamathjayasekara5962@gmail.com>
2021-09-29 10:52:21 +0530

    Daata  base  connection file and test ffle
    Please check your
    url
    usernam
    and
    password and change them accordingly

---
+++ OVRS/src/com/testconnection/ConnectionTest.java
@@ -0,0 +1,22 @@
+package com.testconnection;
+
+import com.connectionDB.*;
+import java.sql.*;
+public class ConnectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Connection conn = DBConnection.getDBConnection();
+	  try {
+		  String query ="select *from user";
+		  Statement statement = conn.createStatement();
+		  ResultSet resultSet = statement.executeQuery(query);
+		  while(resultSet.next()) {
+			  System.out.println(resultSet.getString(1)+" "+resultSet.getString(2));
+		  }
+	  }catch(Exception e) {
+		  System.out.println("Got some error during connectin.");
+	  }
+	}
+
+}
